fec6b8aeacb1ded3f33fc6a237605ddcbb44e3b6
elliot.thuren <elliot.thuren@elev.ga.ntig.se>
2022-03-18 11:18:57 +0100

    🚀

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const testRouter = require('./routes/test');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,5 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 module.exports = app;
---
+++ routes/test.js
@@ -0,0 +1,3 @@
+router.get('/', function (req, res, next) {
+    res.send('Denna route finns på /test/');
+  });
